d5c92d5be5c4cf2a2667c863e84c907c0ef3f41c
Evgenii Taiberi <tayberi00@gmail.com>
2021-02-04 11:28:12 +0500

    Modify Cli.php

--- src/Cli.php
+++ src/Cli.php
@@ -1,19 +1,15 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: taiberi
- * Date: 1/28/21
- * Time: 8:55 AM
- */
-use Php\Project\Lvl1\Cli;
+
+namespace Php\Project\Lvl1\Cli;
 
 use function cli\line;
 use function cli\prompt;
 
 
-function run()
+function run(): string
 {
     line('Welcome to the Brain Game!');
     $name = prompt('May I have your name?');
     line("Hello, %s!", $name);
+    return $name;
 }

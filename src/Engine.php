720d1c776c2f9115d7b99f803a94199841d9d7ac
taiberi <harmankardon00@gmail.com>
2021-05-25 10:40:32 +0500

    refactoring

--- src/Engine.php
+++ src/Engine.php
@@ -12,7 +12,7 @@ function run(callable $generateData, string $description): void
     line('Welcome to the Brain Games!');
 
 
-    $userName = prompt('May I have your name?', false, ' ');
+    $userName = prompt('May I have your name?');
     line("Hello, $userName!", $userName);
 
     line($description);

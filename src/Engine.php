ff8e24917bdcf60c5db89ef2cbbb6bdadfdd68d9
taiberi <harmankardon00@gmail.com>
2021-05-25 09:52:55 +0500

    refactoring Engine.php

--- src/Engine.php
+++ src/Engine.php
@@ -7,31 +7,31 @@ use function cli\prompt;
 
 const MAX_ROUNDS_COUNT = 3;
 
-function run(callable $generateData, string $description)
+function run(callable $generateData, string $description):void
 {
     line('Welcome to the Brain Games!');
 
 
     $userName = prompt('May I have your name?', false, ' ');
-    line("Hello, ${userName}!", $userName);
+    line("Hello, $userName!", $userName);
 
     line($description);
 
     $runRound = function (int $i) use ($generateData, $userName, &$runRound) {
         if ($i >= MAX_ROUNDS_COUNT) {
-            line("Congratulations, ${userName}!");
+            line("Congratulations, $userName!");
             return;
         }
 
         ['question' => $question, 'answer' => $correctAnswer] = $generateData();
 
-        line("Question: ${question}");
+        line("Question: $question");
 
         $answer = prompt('Your answer');
 
         if ($correctAnswer !== $answer) {
-            line("'${answer}' is the wrong answer ;(. The correct answer was '${correctAnswer}'.");
-            line("Let's try again, ${userName}!");
+            line("'$answer' is the wrong answer ;(. The correct answer was '$correctAnswer'.");
+            line("Let's try again, $userName!");
             return;
         }
 
